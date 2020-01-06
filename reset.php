18c9b2dad6756a428e65335092573f2d956257cc
Valentin <val.rossi31795@gmail.com>
2020-01-06 22:47:04 +0100

    save from home

--- reset.php
+++ reset.php
@@ -4,7 +4,9 @@ require "./header.php";
 if (isset($_GET['error']))
 {
     if ($_GET['error'] == 'emptyfield')
-        echo 'Remplissez tous les champs';
+        echo '<div class="alert alert-danger" role="alert">
+        Remplissez tous les champs
+        </div>';
 }
 if (isset($_GET['mail']))
 {
@@ -24,3 +26,28 @@ if (!isset($_GET['username']) && !isset($_GET['error_np']))
 echo '<br><a href="index.php">Revenir à l\'écran de connexion</a>';
 
 ?>
+
+<div class="login-form">
+<form action="./includes/reset_link.inc.php" method="post">
+<h1 class="camagru">Camagru</h1>
+<?php
+if (isset($_GET['error']))
+{
+    if ($_GET['error'] == 'emptyfield')
+        echo '<div class="alert alert-danger" role="alert">
+        Remplissez tous les champs
+        </div>';
+}
+?>
+    <div class="form-group">
+        <input type="text" name="mail" placeholder="E-mail ou nom d'utilisateur" class="form-control">
+    </div>
+    <button class="btn btn-primary btn-block" type="submit" name="reset" class="form-control">Envoyer un lien de connexion</button>
+</form>
+<p class="hr">OU</p>
+<a href="signup.php"><p class="create-account">Créer un compte</p></a>
+<div class="bottom-back">
+<a href="index.php"><p class="back-to-log">Revenir à l'écran de connexion</p></a>
+</div>
+
+</div>
--- signup.php
+++ signup.php
@@ -2,31 +2,59 @@
 <?php
 require "header.php";
 
+?>
+
+<div class="login-form">
+    <h1 class="camagru">Camagru</h1>
+    <form action="includes/signup.inc.php" method="post">
+
+<?PHP
+
 if (isset($_GET['error']))
 {
     if ($_GET['error'] == 'emptyfields')
-    echo 'Remplissez tous les champs';
+    echo '<div class="alert alert-danger" role="alert">
+    Remplissez tous les champs
+    </div>';
     else if ($_GET['error'] == "invalidmailusername")
-        echo 'Email et pseudo invalides';
+        echo '<div class="alert alert-danger" role="alert">
+        Email et pseudo invalides
+        </div>';
     else if ($_GET['error'] == "invalidmail")
-        echo 'Email invalide';
+        echo '<div class="alert alert-danger" role="alert">
+        Email invalide
+        </div>';
     else if ($_GET['error'] == "invalidusername")
-        echo 'Pseudo invalide';
+        echo '<div class="alert alert-danger" role="alert">
+        Pseudo invalide
+        </div>';
     else if ($_GET['error'] == "passwordcheck")
-        echo 'Les mots de passe ne correspondent pas';
+        echo '<div class="alert alert-danger" role="alert">
+        Les mots de passe ne correspondent pas
+        </div>';
     if ($_GET['error'] == "usertaken")
-        echo 'Pseudo déjà existant';
+        echo '<div class="alert alert-danger" role="alert">
+        Pseudo déjà existant
+        </div>';
     if ($_GET['error'] == "emailtaken")
-        echo 'Email déjà existant';
+        echo '<div class="alert alert-danger" role="alert">
+        Email déjà existant
+        </div>';
 }
 
 ?>
 
-<form action="includes/signup.inc.php" method="post">
-    <input type="email" name="mail" placeholder="E-mail">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="pwd" placeholder="Password" minlength='8'>
-    <input type="password" name="pwd2" placeholder="Repeat password" minlength='8'>
-    <button class="btn" type="submit" name="signup">Signup</button>
-</form>
-<p>Vous avez un compte ? <a href="index.php">Connectez-vous</a></p>
+    <div class="form-group">
+        <input style="margin-bottom:5px;" type="email" name="mail" placeholder="E-mail" class="form-control">
+        <input style="margin-bottom:5px;" type="text" name="username" placeholder="Username" class="form-control">
+        <input style="margin-bottom:5px;" type="password" name="pwd" placeholder="Password" minlength='8' class="form-control">
+        <input type="password" name="pwd2" placeholder="Repeat password" minlength='8' class="form-control">
+    </div>
+    <button class="btn btn-primary btn-block" type="submit" name="signup">Signup</button>
+    </form>
+    <p class="hr">OU</p>
+    Vous avez un compte ? <a href="index.php">Connectez-vous</a>
+
+
+</div>
+
